f20edc5fb8e652c65df852f0c188b29d768a23de
diego cordeiro <diegocordeiro121@gmail.com>
2024-01-25 16:47:12 -0300

    feat: (req4) liberando a visualização da viewset de Profile para todos e restringindo o acesso aos demais métodos

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 from .models import Profile, Project
 
 
+# class AdminProfileSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Profile
+#         fields = '__all__'
+
+#     def create(self, validated_data):
+#         request = self.context.get('request')
+#         if request and request.user.is_superuser:
+#             return super().create(validated_data)
+#         raise serializers.ValidationError(
+#             "Você não tem permissão para criar fornecedores."
+#           )
+
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
