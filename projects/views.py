805703fa44464f0d8a6abd872e902c30b035e337
diego cordeiro <diegocordeiro121@gmail.com>
2024-01-27 19:17:24 -0300

    styles: estilizando a aba projetos(inacabado)

--- projects/views.py
+++ projects/views.py
@@ -25,11 +25,24 @@ class ProfileViewSet(viewsets.ModelViewSet):
         if request.method == 'GET' and pk is not None:
             profile_data = Profile.objects.get(pk=pk)
             projects = Project.objects.filter(profile=pk)
+            print(projects)
+            projects_formated_list = list()
+            for project in projects:
+                key_skill_formated = project.key_skill.split(",")
+                project_formated = {
+                    "name": project.name,
+                    "description": project.description,
+                    "github_url": project.github_url,
+                    "keyword": project.keyword,
+                    "key_skill": key_skill_formated
+                }
+                projects_formated_list.append(project_formated)
+
             certificates = Certificate.objects.filter(profiles=pk)
             context = {
                 "profile": profile_data,
-                "projects": projects,
-                "certificates": certificates
+                "projects": projects_formated_list,
+                "certificates": certificates,
             }
             return render(request, "profile_detail.html", context)
         return super().retrieve(request, *args, **kwargs)
